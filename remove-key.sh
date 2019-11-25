5577239288a5a80330e33744ab6d15894a396763
Théo <theo.huchard@gmail.com>
2019-11-25 11:53:08 +0100

    Update remove-key.sh and remove-repository.sh

--- remove-key.sh
+++ remove-key.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 sudo apt-key list
+echo "Choose the key to delete (copy and paste the full key):"
 read key_to_delete
-udo apt-key del $key_to_delete
+echo "Deleting $key_to_delete ..."
+sudo apt-key del "$key_to_delete"
+sudo apt update
+
--- remove-repository.sh
+++ remove-repository.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 grep ^ /etc/apt/sources.list /etc/apt/sources.list.d/*
+echo "Choose the repository to delete (e.g \"ppa:nemh/systemback\")"
 read repository_to_delete
+echo "Deleting the repository $repository_to_delete"
 sudo add-apt-repository -r $repository_to_delete
+echo "OK"
