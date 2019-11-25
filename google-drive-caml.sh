fda1f88c10aa6c2c3bc111367f12043b70429adf
Théo Huchard <26305730+TheoHd@users.noreply.github.com>
2019-11-25 10:56:23 +0100

    Create google-drive-caml.sh

---
+++ google-drive-caml.sh
@@ -0,0 +1,5 @@
+sudo add-apt-repository ppa:alessandro-strada/ppa
+sudo apt update && sudo apt install google-drive-ocamlfuse
+google-drive-ocamlfuse
+mkdir -p ~/google-drive
+google-drive-ocamlfuse ~/google-drive
